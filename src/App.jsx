3f2c3bb381b65e4931fd2f4c0953b1b034eb3d85
Psycho <ilya.efremov.0498@gmail.com>
2024-10-10 20:17:49 +0500

    Init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Store from './Products/Store.jsx';
+function App() {
+    return (
+        <div className='App'>
+            <Store />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Products/ShopItem/ShopItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ShopItem.css';
+
+export default function ShopItem({ item }) {
+  const { name, price, color, img } = item;
+  return (
+    <li className={'ShopItem'}>
+      <img src={img} alt={name} className={'ShopItem__img'} />
+      <h2 className={'ShopItem__title'}>{name}</h2>
+      <h3 className={'ShopItem_subtitle'}>{color}</h3>
+      <span className={'ShopItem_price'}>${price}</span>
+    </li>
+  );
+}
